1adc6df3e4b3c7387087b6c94673a70db41de444
Adrian Yorke <adrianyorke@gmail.com>
2018-04-22 01:22:39 +0300

    Split tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,9 +1,11 @@
 import unittest
 
 from pipeline import pipeline
-    
-def test_status():
+
+def test_pipeline_status_valid():
     output = pipeline.getPipelineStatus("Staging")
     assert output is not None
+
+def test_pipeline_status_invalid():
     output = pipeline.getPipelineStatus("Invalid")
     assert output is None
